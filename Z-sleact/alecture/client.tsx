9dc625ea6cbbb1c8b455de02137e83bb23684919
junmiLee <junmile@ef-ems.com>
2024-06-25 11:16:43 +0900

    📚⚒ TS + webpack 실행하기

--- Z-sleact/alecture/client.tsx
+++ Z-sleact/alecture/client.tsx
@@ -1,7 +1,15 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from '@layouts/App';
+import axios from 'axios';
+
+import App from './layouts/App';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'production' ? 'https://sleact.nodebird.com' : 'http://localhost:3090';
 
 render(
   <BrowserRouter>
@@ -9,7 +17,3 @@ render(
   </BrowserRouter>,
   document.querySelector('#app'),
 );
-
-// pages - 서비스 페이지
-// components - 짜잘 컴포넌트
-// layouts - 공통 레이아웃
