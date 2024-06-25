fcfeae5e6442af3c270a432eebc8afef2bf31e09
junmiLee <junmile@ef-ems.com>
2024-06-25 14:02:55 +0900

    📚⚒ 폴더 구조와 리액트 라우터

--- Z-sleact/alecture/layouts/App.tsx
+++ Z-sleact/alecture/layouts/App.tsx
@@ -1,7 +1,16 @@
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 const App = () => {
-  return <div>안녕하세요, 테스트 페이지입니다!!</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="login" />
+      <Route path="/login" component={Login} />
+      <Route path="/sign-up" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
